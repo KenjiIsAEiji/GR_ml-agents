3a133b8a1b06078abf4d9125385347385e00ddd1
eiji_miyazawa <b1800722@planet.kanazawa-it.ac.jp>
2021-06-30 17:03:16 +0900

    Agentクラスを継承した形に変更

--- GR_ML-Agents_UnityProject/Assets/Scripts/shooterAI/ShooterAgent.cs
+++ GR_ML-Agents_UnityProject/Assets/Scripts/shooterAI/ShooterAgent.cs
@@ -1,41 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
 
-public class ShooterAgent : MonoBehaviour
+public class ShooterAgent : Agent
 {
+    [Header("-- Agent moving settings --")]
+    [SerializeField] float moveSpeed = 5.0f;
+    [SerializeField] float rotateSpeed = 5.0f;
+    Rigidbody agentRb;
+    
+    [Header("-- Game management --")]
     [SerializeField] GameManager gameManager;
+
+    [Header("-- Agent bullet shooting settings --")]
     [SerializeField] GameObject bulletPrefab;
     [SerializeField] float firePower = 10f;
     [SerializeField] float coolTime = 2.0f;
-    private bool firing = false;
     [SerializeField] Transform muzzleTransform;
-    
-    public int agentId;
+    private bool firing = false;
 
-    Rigidbody agentRb;
-    [SerializeField] float moveSpeed = 5.0f;
-    [SerializeField] float rotateSpeed = 5.0f;
+    [Header("-- Agents Settings --")]
+    public int agentId;
+    public Transform opponentTransform;
 
     // Start is called before the first frame update
-    void Start()
+    public override void Initialize()
     {
-        agentRb = GetComponent<Rigidbody>();
+        this.agentRb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // 
+    public override void CollectObservations(VectorSensor sensor)
     {
-        // rotate
-        Vector3 rotatingVec = Vector3.zero;
-        if(Input.GetKey(KeyCode.J)) rotatingVec += Vector3.down;
-        if(Input.GetKey(KeyCode.L)) rotatingVec += Vector3.up;
-        // Debug.Log(rotatingVec);
+        sensor.AddObservation(this.transform.position.x);
+        sensor.AddObservation(this.transform.position.z);
 
+        sensor.AddObservation(opponentTransform.position.x);
+        sensor.AddObservation(opponentTransform.position.z);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        // actions organizing ---------------------------------
+        int verticalAction = (int)actions.DiscreteActions[0];
+        int horizonAction = (int)actions.DiscreteActions[1];
+        int rotateAction = (int)actions.DiscreteActions[2];
+        int fire = (int)actions.DiscreteActions[3];
+        
+        // movement ---------------------------------
+        Vector3 vel = Vector3.zero;
+
+        if(verticalAction == 1) vel.z += 1.0f;
+        if(verticalAction == 2) vel.z += -1.0f;
+
+        if(horizonAction == 1) vel.x += -1.0f;
+        if(horizonAction == 2) vel.x += 1.0f;
+
+        vel = vel * moveSpeed;
+        // Debug.Log(vel);
+        agentRb.AddRelativeForce(vel * agentRb.mass * agentRb.drag / (1f - agentRb.drag * Time.fixedDeltaTime));
+
+        // rotate ---------------------------------
+        Vector3 rotatingVec = Vector3.zero;
+        if(rotateAction == 1) rotatingVec += Vector3.down;
+        if(rotateAction == 2) rotatingVec += Vector3.up;
+        // Debug.Log(rotatingVec);
         this.transform.Rotate(rotatingVec * rotateSpeed * Time.deltaTime);
 
-        // bullet firing
-        if(Input.GetKey(KeyCode.Space)){
+        // bullet firing ---------------------------------
+        if(fire == 1){
             //Debug.Log("trigger down");
             if(!firing){
                 StartCoroutine(FireTimer());
@@ -43,24 +79,89 @@ public class ShooterAgent : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
+    public override void Heuristic(in ActionBuffers actionsOut)
     {
-        // movement
-        Vector3 vel = Vector3.zero;
-
-        if(Input.GetKey(KeyCode.W)) vel.z += 1.0f;
+        var discreteActions = actionsOut.DiscreteActions;
         
-        if(Input.GetKey(KeyCode.S)) vel.z += -1.0f;
-        
-        if(Input.GetKey(KeyCode.A)) vel.x += -1.0f;
-        
-        if(Input.GetKey(KeyCode.D)) vel.x += 1.0f;
+        // vertical
+        if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S)){
+            discreteActions[0] = 0;
+        }else if(Input.GetKey(KeyCode.W)){
+            discreteActions[0] = 1;
+        }else if(Input.GetKey(KeyCode.S)){
+            discreteActions[0] = 2;
+        }else{
+            discreteActions[0] = 0;
+        }
 
-        vel = vel * moveSpeed;
-        // Debug.Log(vel);
+        // horizon
+        if(Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)){
+            discreteActions[1] = 0;
+        }else if(Input.GetKey(KeyCode.A)){
+            discreteActions[1] = 1;
+        }else if(Input.GetKey(KeyCode.D)){
+            discreteActions[1] = 2;
+        }else{
+            discreteActions[1] = 0;
+        }
 
-        agentRb.AddRelativeForce(vel * agentRb.mass * agentRb.drag / (1f - agentRb.drag * Time.fixedDeltaTime));
+        // rotate
+        if(Input.GetKey(KeyCode.J) && Input.GetKey(KeyCode.L)){
+            discreteActions[2] = 0;
+        }else if(Input.GetKey(KeyCode.J)){
+            discreteActions[2] = 1;
+        }else if(Input.GetKey(KeyCode.L)){
+            discreteActions[2] = 2;
+        }else{
+            discreteActions[2] = 0;
+        }
+
+        // firing
+        if(Input.GetKey(KeyCode.Space)){
+            discreteActions[3] = 1;
+        }else{
+            discreteActions[3] = 0;
+        }
     }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     // rotate
+    //     Vector3 rotatingVec = Vector3.zero;
+    //     if(Input.GetKey(KeyCode.J)) rotatingVec += Vector3.down;
+    //     if(Input.GetKey(KeyCode.L)) rotatingVec += Vector3.up;
+    //     // Debug.Log(rotatingVec);
+
+    //     this.transform.Rotate(rotatingVec * rotateSpeed * Time.deltaTime);
+
+    //     // bullet firing
+    //     if(Input.GetKey(KeyCode.Space)){
+    //         //Debug.Log("trigger down");
+    //         if(!firing){
+    //             StartCoroutine(FireTimer());
+    //         }
+    //     }
+    // }
+
+    // void FixedUpdate()
+    // {
+    //     // movement
+    //     Vector3 vel = Vector3.zero;
+
+    //     if(Input.GetKey(KeyCode.W)) vel.z += 1.0f;
+        
+    //     if(Input.GetKey(KeyCode.S)) vel.z += -1.0f;
+        
+    //     if(Input.GetKey(KeyCode.A)) vel.x += -1.0f;
+        
+    //     if(Input.GetKey(KeyCode.D)) vel.x += 1.0f;
+
+    //     vel = vel * moveSpeed;
+    //     // Debug.Log(vel);
+
+    //     agentRb.AddRelativeForce(vel * agentRb.mass * agentRb.drag / (1f - agentRb.drag * Time.fixedDeltaTime));
+    // }
 
     void OnCollisionEnter(Collision collisionInfo)
     {
